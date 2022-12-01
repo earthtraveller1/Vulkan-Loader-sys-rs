304c540064b81ddf0679a395e66c9ae732ff3d76
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-30 19:42:52 -0500

    Included the generated header.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+#![allow(nonstandard_style)]
+
+include!(concat!(env!("OUT_DIR"), "/vulkan.rs"));
