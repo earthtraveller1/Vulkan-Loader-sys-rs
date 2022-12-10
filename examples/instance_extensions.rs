8c1f0fb8366ea23fd9ba7e1a23708f90c36c286f
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-12-09 19:10:23 -0500

    Added a simple program that just retrieves the instance extensions.

---
+++ examples/instance_extensions.rs
@@ -0,0 +1,21 @@
+use std::{ptr::{null, null_mut},ffi::CStr};
+use vulkan_loader_sys::*;
+
+fn main() {
+    unsafe {
+        let mut extension_count = 0;
+        vkEnumerateInstanceExtensionProperties(null(), &mut extension_count, null_mut());
+
+        let mut extensions = Vec::with_capacity(extension_count.try_into().unwrap());
+        vkEnumerateInstanceExtensionProperties(null(), &mut extension_count, extensions.as_mut_ptr());
+        extensions.set_len(extension_count.try_into().unwrap());
+        
+        
+        extensions.iter().for_each(|extension| {
+            println!(
+                "[INFO]: Found instance extension {}.",
+                CStr::from_ptr(&extension.extensionName[0]).to_str().unwrap()
+            );
+        });
+    }
+}
