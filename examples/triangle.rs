76f6535632a8d6cb0d494e0dea31a9f8dcecbba9
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-12-29 17:58:57 -0500

    Chose a physical device.

--- examples/triangle.rs
+++ examples/triangle.rs
@@ -1,5 +1,8 @@
-use std::ffi::CString;
-use std::ptr::{null, null_mut};
+use std::{
+    ffi::{CStr, CString},
+    mem::MaybeUninit,
+    ptr::{null, null_mut},
+};
 /// Designed to be a basic Vulkan application.
 /// Would probably display a triangle some time in the future, but for now it
 /// is just Vulkan boilerplate code.
@@ -62,19 +65,120 @@ unsafe fn create_instance(glfw: &glfw::Glfw) -> VkInstance {
     instance
 }
 
+fn create_surface(instance: VkInstance, window: &glfw::Window) -> VkSurfaceKHR {
+    let mut surface = null_mut::<VkSurfaceKHR_T>();
+    window.create_window_surface(
+        instance as usize,
+        null(),
+        &mut surface as *mut VkSurfaceKHR as *mut u64,
+    );
+    surface
+}
+
+unsafe fn choose_physical_device(
+    instance: VkInstance,
+    surface: VkSurfaceKHR,
+) -> (VkPhysicalDevice, u32, u32) {
+    let mut physical_device_count = 0;
+    vkEnumeratePhysicalDevices(instance, &mut physical_device_count, null_mut());
+
+    let mut physical_devices = Vec::with_capacity(physical_device_count.try_into().unwrap());
+    vkEnumeratePhysicalDevices(
+        instance,
+        &mut physical_device_count,
+        physical_devices.as_mut_ptr(),
+    );
+    physical_devices.set_len(physical_device_count.try_into().unwrap());
+
+    let find_queue_families = |device: VkPhysicalDevice| -> (Option<u32>, Option<u32>) {
+        let mut queue_family_count = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &mut queue_family_count, null_mut());
+        let mut queue_family_properties =
+            Vec::with_capacity(queue_family_count.try_into().unwrap());
+        vkGetPhysicalDeviceQueueFamilyProperties(
+            device,
+            &mut queue_family_count,
+            queue_family_properties.as_mut_ptr(),
+        );
+        queue_family_properties.set_len(queue_family_count.try_into().unwrap());
+
+        let mut graphics_family = None;
+        let mut present_family = None;
+
+        for i in 0..queue_family_properties.len() {
+            if (queue_family_properties[i].queueFlags & VK_QUEUE_GRAPHICS_BIT as u32) != 0 {
+                graphics_family = Some(i.try_into().unwrap());
+            }
+
+            let mut present_support = VK_FALSE;
+            vkGetPhysicalDeviceSurfaceSupportKHR(
+                device,
+                i.try_into().unwrap(),
+                surface,
+                &mut present_support,
+            );
+
+            if present_support == VK_TRUE {
+                present_family = Some(i.try_into().unwrap());
+            }
+        }
+
+        (graphics_family, present_family)
+    };
+
+    let physical_device = *physical_devices
+        .iter()
+        .find(|device| {
+            let (graphics_family, present_family) = find_queue_families(**device);
+
+            graphics_family.is_some() && present_family.is_some()
+        })
+        .expect("Could not find an adequate physical device.");
+
+    let (graphics_family, present_family) = find_queue_families(physical_device);
+    let graphics_family = graphics_family.unwrap();
+    let present_family = present_family.unwrap();
+
+    let mut device_properties = MaybeUninit::uninit();
+    vkGetPhysicalDeviceProperties(physical_device, device_properties.as_mut_ptr());
+    let device_properties = device_properties.assume_init();
+
+    println!(
+        "[INFO]: Using the {} graphics card.",
+        CStr::from_ptr(&device_properties.deviceName[0])
+            .to_str()
+            .unwrap()
+    );
+
+    (physical_device, graphics_family, present_family)
+}
+
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).expect("Failed to initialize GLFW.");
     glfw.window_hint(glfw::WindowHint::ClientApi(glfw::ClientApiHint::NoApi));
     glfw.window_hint(glfw::WindowHint::Resizable(false)); // Handling resizing is a bit complicated in Vulkan, so I'll disable it for now.
-    
+
     let instance = unsafe { create_instance(&glfw) };
-    let (window, _) = glfw.create_window(WINDOW_WIDTH, WINDOW_HEIGHT, "Triangle Example", glfw::WindowMode::Windowed).expect("Failed to create the GLFW window.");
-    
+    let (window, _) = glfw
+        .create_window(
+            WINDOW_WIDTH,
+            WINDOW_HEIGHT,
+            "Triangle Example",
+            glfw::WindowMode::Windowed,
+        )
+        .expect("Failed to create the GLFW window.");
+
+    let surface = create_surface(instance, &window);
+
+    let (physical_device, graphics_queue_family, present_queue_family) =
+        unsafe { choose_physical_device(instance, surface) };
+
     while !window.should_close() {
         glfw.poll_events();
     }
-    
+
     unsafe {
+        vkDestroySurfaceKHR(instance, surface, null());
         vkDestroyInstance(instance, null());
     }
 }
